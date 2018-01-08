e8c2e976ba576147ab70e7979c546a8504b0a009
gracz21 <kamil.walkowiak@netguru.pl>
2018-01-08 12:38:32 +0100

    Add Github pull request webhook endpoint

--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
+require 'atlassian/jwt'
 require 'json'
+require 'net/http'
 require 'pry'
 require 'sinatra'
+require 'uri'
+
+require_relative 'services/github/pull_request_parser'
 
 post '/installed' do
   request.body.rewind
@@ -8,4 +13,13 @@ post '/installed' do
   puts request_payload['sharedSecret']
 end
 
+post '/pull_request_changed' do
+  request.body.rewind
+  request_payload = JSON.parse(request.body.read)
+  return unless %w[opened reopened closed].include?(request_payload['action'])
+
+  parsed_payload = Github::PullRequestParser
+                   .new(github_payload: request_payload)
+                   .call
+  parsed_payload.to_json
 end
