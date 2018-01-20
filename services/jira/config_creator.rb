2ba4e94bd5c86e834ab1e81f08deb70541747687
gracz21 <kamil.walkowiak@netguru.pl>
2018-01-20 22:36:04 +0100

    Add Jira config creator service

---
+++ services/jira/config_creator.rb
@@ -0,0 +1,25 @@
+module Jira
+  class ConfigCreator
+    def initialize(installation_payload:)
+      @installation_payload = installation_payload
+    end
+
+    def call
+      JiraConfig.create!(parse_installation_payload)
+    end
+
+    private
+
+    attr_reader :installation_payload, :fields_payload
+
+    
+
+    def parse_installation_payload
+      {
+        client_key: installation_payload['baseUrl'],
+        url: installation_payload['baseUrl'],
+        shared_secret: installation_payload['sharedSecret']
+      }
+    end
+  end
+end
