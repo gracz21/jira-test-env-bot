54d31ccd5683f6f2a7b07d42849d898c41b4d5eb
gracz21 <kamil.walkowiak@netguru.pl>
2018-02-26 22:05:33 +0100

    Move request payload parser to before endpoint action

--- app.rb
+++ app.rb
@@ -16,22 +16,23 @@ require_relative 'services/github/pull_request_parser'
 require_relative 'services/jira/config_creator'
 require_relative 'services/jira/test_environment_updater'
 
-post '/installed' do
+before do
   request.body.rewind
-  request_payload = JSON.parse(request.body.read)
-  Jira::ConfigCreator.new(installation_payload: request_payload).call
+  @request_payload = JSON.parse(request.body.read)
+end
+
+post '/installed' do
+  Jira::ConfigCreator.new(installation_payload: @request_payload).call
 end
 
 post '/uninstalled' do
 end
 
 post '/pull_request_changed' do
-  request.body.rewind
-  request_payload = JSON.parse(request.body.read)
-  halt 204 unless %w[opened reopened closed].include?(request_payload['action'])
+  halt 204 unless %w[opened reopened closed].include?(@request_payload['action'])
 
   parsed_payload = Github::PullRequestParser
-                   .new(github_payload: request_payload)
+                   .new(github_payload: @request_payload)
                    .call
   results = Jira::TestEnvironmentUpdater.new(parsed_payload).call
   status results[:code]
