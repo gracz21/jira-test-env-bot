4823ff87f1ca78d4834166205418a9f2f7074b86
gracz21 <kamil.walkowiak@netguru.pl>
2018-05-12 19:44:55 +0200

    Add index jira configs endpoint

--- app/controllers/jira_configs.rb
+++ app/controllers/jira_configs.rb
@@ -1,4 +1,9 @@
 class App
+  get '/jira_configs' do
+    @jira_configs = JiraConfig.all
+    haml :'jira_configs/index'
+  end
+
   post '/installed' do
     Jira::ConfigCreator.new(installation_payload: @request_payload).call
   end
