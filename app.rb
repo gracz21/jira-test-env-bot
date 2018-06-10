be21c1f9bdd1b6aa4e75f9e32f7099e988605bd7
gracz21 <kamil.walkowiak@netguru.pl>
2018-06-10 14:57:54 +0200

    Enable request method override

--- app.rb
+++ app.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   register WillPaginate::Sinatra
 
+  set :method_override, true
   set :root, File.dirname(__FILE__)
   set :haml, format: :html5, layout: :application
   set :sprockets, Sprockets::Environment.new
